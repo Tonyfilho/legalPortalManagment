6f82c7bb37b7148feab3f57bc0a39419d195cbcc
unknown <oficinadosdrones@gmail.com>
2022-03-10 21:43:13 +0000

    imagem do moongy-company ficou dinamica

--- legalPortalManagment/src/app/moongy-group/moongy-company/moongy-company.component.ts
+++ legalPortalManagment/src/app/moongy-group/moongy-company/moongy-company.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoongyCompanyComponent implements OnInit {
    companyName:string = 'Adentis';
+   serverImage:string = '../../../assets/images/moongyAdentis.png'; //esta imagem virá de forma dinamica da DB;
   constructor() { }
 
   ngOnInit(): void {
