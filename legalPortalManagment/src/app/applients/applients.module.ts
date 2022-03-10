ef7997102ff3688325fdbd8224d9a48e1e20b910
unknown <oficinadosdrones@gmail.com>
2022-03-10 12:29:40 +0000

    serviço e modulo e componente do applients feito

---
+++ legalPortalManagment/src/app/applients/applients.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApplientsComponent } from './applients/applients.component';
+
+
+
+@NgModule({
+  declarations: [
+    ApplientsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ApplientsModule { }
---
+++ legalPortalManagment/src/app/applients/applients/applients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applients',
+  templateUrl: './applients.component.html',
+  styleUrls: ['./applients.component.css']
+})
+export class ApplientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
