f737ac28f20b3c4ae1deb7338074d07b8a02bddf
unknown <oficinadosdrones@gmail.com>
2022-03-10 14:22:54 +0000

    criando compoment moongy-company

---
+++ legalPortalManagment/src/app/moongy-group/moongy-company/moongy-company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moongy-company',
+  templateUrl: './moongy-company.component.html',
+  styleUrls: ['./moongy-company.component.css']
+})
+export class MoongyCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- legalPortalManagment/src/app/moongy-group/moongy-group.module.ts
+++ legalPortalManagment/src/app/moongy-group/moongy-group.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './moongy-user/user.component';
+import { MoongyCompanyComponent } from './moongy-company/moongy-company.component';
 
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    MoongyCompanyComponent
   ],
   imports: [
     CommonModule
