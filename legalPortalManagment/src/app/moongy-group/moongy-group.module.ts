5118ff0b7399931e455b01030b92e8b6f8a2a58a
unknown <oficinadosdrones@gmail.com>
2022-03-10 14:17:50 +0000

    Acerrtando diretorios moongyGroup

--- legalPortalManagment/src/app/moongy-group/moongy-group.module.ts
+++ legalPortalManagment/src/app/moongy-group/moongy-group.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserComponent } from './moongy-company/moongy-user/user.component';
+import { UserComponent } from './moongy-user/user.component';
 
 
 
