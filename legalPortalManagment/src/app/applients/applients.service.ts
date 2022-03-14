e3c1b1c76929b70b4a16385ecb331df6104ec406
TonyFilho <oficinadosdrones@gmail.com>
2022-03-14 12:55:13 +0000

    serviço pronto e não testado

--- legalPortalManagment/src/app/applients/applients.service.ts
+++ legalPortalManagment/src/app/applients/applients.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject, take } from 'rxjs';
+import { ApplientClass } from 'src/assets/class/appient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApplientsService {
 
-  constructor() { }
+  private localId = new BehaviorSubject<number>(0);
+  serviceIdReturn = this.localId.asObservable();
+  
+  applients:ApplientClass[] = [];
+
+
+  constructor(private httpClient: HttpClient) {}
+
+  getApplients() {
+    const localApplients = this.httpClient.get(
+      `https://jsonplaceholder.typicode.com/users`
+    );
+    return localApplients.pipe(take(1)) && this.applients;
+  }
+  getApplientById(id: number) {
+    const localApplients = this.httpClient.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    return localApplients.pipe(take(1))  && this.applients[id].id;
+  }
+
+  getId(id: number): void {
+    //emite um Observable do ID
+    this.localId.next(id);
+  }
+
+  postUser(applient: ApplientClass) {
+    this.applients.push(applient);
+    return this.httpClient.post(
+      `https://httpbin.org/post`,
+      JSON.stringify(applient)
+    ).pipe(take(1)); //JSON é uma API de JSON q o browser vai usar, o stringify é o metodo de conversão de Objeto JS / JSON.
+  }
 }
---
+++ legalPortalManagment/src/assets/class/appient.ts
@@ -0,0 +1,47 @@
+export class ApplientClass {
+    id!: number;
+    name!: string;
+    email!: string;
+    admissionDate!: Date;
+    birthDate!: Date;
+    phone!: string;
+    emergencyContactPhone!: string;
+    emergencyContactName!: string;
+    degreeOfkinShip!: string;
+    naturalness!: string;
+    nationality!: string;
+    address!:string;
+    zipeCode!:string;
+    locality!:string;
+    documentNumber!:string;
+    documentValidityDate!:Date;
+    admissionPlace!:string;
+    internationalID!:string;
+    internationalValidity!:string;
+    internationalEmitDate!:string;
+    passportNumber!:string;
+    passportValidity!:string;
+    passportExpeditionDate!:Date;
+    vatNumber!:string;
+    socialSecurity!:string;
+    maritalStatus!:string;
+    numberOfDependents!:string;
+    bankName!:string;
+    swiftName!:string;
+    bicNumber!:string;
+    nibNumber!: string;
+    registrationVehicleNumber!:string;
+
+
+
+
+
+
+
+    
+
+
+
+
+
+}
